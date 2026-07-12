35abf6b7d3ef320ad86e6efe811235950cc36874
zydingg <102214148+zydingg@users.noreply.github.com>
2026-07-12 20:08:13 +0800

    chore: upload vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,4 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({ plugins: [react()], server: { proxy: { '/api': 'http://localhost:8787' } } });
